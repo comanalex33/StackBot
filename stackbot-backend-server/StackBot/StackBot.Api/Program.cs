1896e7e6e3fec8182b012311381e7619f542eb6a
Cătălin Secoșan <secosan.catalinluca@gmail.com>
2024-05-11 15:17:01 +0300

    Implemented the required infrastructure for JWT Authorization

--- stackbot-backend-server/StackBot/StackBot.Api/Program.cs
+++ stackbot-backend-server/StackBot/StackBot.Api/Program.cs
@@ -1,5 +1,4 @@
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
 using Newtonsoft.Json;
 using Stackbot.DataAccess;
 using StackBot.Api.Models;
@@ -15,7 +14,7 @@ builder.Services.AddSwaggerGen();
 
 var config = new ConfigurationBuilder()
     .SetBasePath(Directory.GetCurrentDirectory())
-    .AddJsonFile("appsettings.Development.json")
+    .AddJsonFile("appsettings.json")
     .Build();
 
 using StreamReader r = new(config.GetValue<string>("SecretsFile"));
---
+++ stackbot-backend-server/StackBot/StackBot.Business/Services/IdentityService.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.Extensions.Options;
+using StackBot.Api.Options;
+using System.Text;
+using System.IdentityModel.Tokens.Jwt;
+using Microsoft.IdentityModel.Tokens;
+using System.Security.Claims;
+
+namespace StackBot.Business.Services
+{
+    public class IdentityService
+    {
+        private readonly JwtSettings? _settings;
+        private readonly byte[] _key;
+
+        public IdentityService(IOptions<JwtSettings> jwtOptions)
+        {
+            _settings = jwtOptions.Value;
+            ArgumentNullException.ThrowIfNull(_settings);
+            ArgumentNullException.ThrowIfNull(_settings.SigningKey);
+            ArgumentNullException.ThrowIfNull(_settings.Audiences);
+            ArgumentNullException.ThrowIfNull(_settings.Audiences[0]);
+            ArgumentNullException.ThrowIfNull(_settings.Issuer);
+            _key = Encoding.ASCII.GetBytes(_settings?.SigningKey!);
+        }
+
+        public JwtSecurityTokenHandler TokenHandler = new JwtSecurityTokenHandler();
+    
+        public SecurityToken CreateSecurityToken(ClaimsIdentity identity)
+        {
+            var tokenDescription = GetTokenDescriptor(identity);
+            return TokenHandler.CreateToken(tokenDescription);
+        }
+
+        public string WriteToken(SecurityToken token)
+        {
+            return TokenHandler.WriteToken(token);
+        }
+
+        private SecurityTokenDescriptor GetTokenDescriptor(ClaimsIdentity identity)
+        {
+            return new SecurityTokenDescriptor()
+            {
+                Subject = identity,
+                Expires = DateTime.Now.AddHours(2),
+                Audience = _settings!.Audiences?[0]!,
+                Issuer = _settings.Issuer,
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(_key),
+                    SecurityAlgorithms.HmacSha256Signature)
+            };
+        }
+    }
+}
